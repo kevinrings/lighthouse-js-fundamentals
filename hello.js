d1681f32b5206e171e26b846d505e403dcc1ba2c
kevinrings <wnkevin@gmail.com>
2023-04-09 14:55:56 -0700

    update

--- hello.js
+++ hello.js
@@ -11,3 +11,15 @@ const sayHello  = function (name) {
   console.log("Hello, " + name);
 }
 
+// console.log
+const sayHelloToConsole  = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John'); 
+
+//return
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
